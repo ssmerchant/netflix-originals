c35cb5034ffead0bc7e4cd9386d80459dc26fa76
ssmerchant <103539397+ssmerchant@users.noreply.github.com>
2022-06-08 19:56:37 -0500

    Add files via upload

--- NetflixOriginalFilms/RuntimeIMDBcorr.py
+++ NetflixOriginalFilms/RuntimeIMDBcorr.py
@@ -1,9 +1,10 @@
 import pandas as pd
 
 #read Runtime and IMDB Scores from csv
-fields = ['Runtime', 'IMDB Score']
+fields = ['Runtime','Year', 'IMDB Score']
 data = pd.read_csv(r'D:\\Projects\\NetflixOriginalFilms\\NetflixOriginalsCSV.csv', usecols = fields, encoding = 'latin1')
 
 #Finding correlation between Runtime/IMDB Scores
 corr = data.loc[:,fields].corr()
+
 print(corr)
